261afdc22c8141d769b1930189d9b68ca68eca8d
Martin Sumner <martin.sumner@adaptip.co.uk>
2026-04-14 13:18:51 +0100

    compile URL separator

--- src/riak_api_web_socket.erl
+++ src/riak_api_web_socket.erl
@@ -244,7 +244,8 @@ init(Options) ->
         [IP, Port, StartSize]
     ),
     riak_api_web:cache_today(),
-    riak_api_web_headers:compile_separator(),
+    riak_api_web_headers:compile_separators(),
+    riak_api_web_acceptor:compile_separators(),
     {
         ok,
         #socket_state{
