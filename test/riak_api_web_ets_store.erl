6801f85e6c9159a4e2bfb435a9283fd1aefaf7e3
Martin Sumner <martin.sumner@adaptip.co.uk>
2026-06-09 13:08:27 +0100

    Add temp logs to try and detect point of intermittent failure

--- test/riak_api_web_ets_store.erl
+++ test/riak_api_web_ets_store.erl
@@ -592,7 +592,12 @@ receive_loop(_Msg, 0) ->
     ok;
 receive_loop(Msg, Expected) ->
     receive
-        Msg ->
+        {Msg, Rsp} ->
+            io:format(
+                user,
+                "Msg ~w acknowledged from ~w ~w~n",
+                [Msg, Rsp, Expected]
+            ),
             receive_loop(Msg, Expected - 1)
     end.
 
@@ -633,9 +638,10 @@ continue_request(IP, Port, Key, Length, Sleep, ReportPID) ->
     ok = gen_tcp:send(Socket, BinRq),
     {ok, <<"HTTP/1.1 100 Continue\r\n\r\n">>} =
         gen_tcp:recv(Socket, 0, 100),
+    io:format(user, "Continue received ~w~n", [self()]),
     case is_pid(ReportPID) of
         true ->
-            ReportPID ! continue_received;
+            ReportPID ! {continue_received, self()};
         _ ->
             ok
     end,
@@ -643,6 +649,7 @@ continue_request(IP, Port, Key, Length, Sleep, ReportPID) ->
     Value = crypto:strong_rand_bytes(Length),
     ok = gen_tcp:send(Socket, Value),
     {ok, Rsp} = gen_tcp:recv(Socket, 0),
+    io:format(user, "Response received at ~w~n", [self()]),
     {ok, Status, Rest} = erlang:decode_packet(line, Rsp, []),
     ?assertMatch(
         <<"HTTP/1.1 204 No Content\r\n">>,
@@ -659,7 +666,7 @@ continue_request(IP, Port, Key, Length, Sleep, ReportPID) ->
     ok = gen_tcp:close(Socket),
     case is_pid(ReportPID) of
         true ->
-            ReportPID ! complete;
+            ReportPID ! {complete, self()};
         _ ->
             ok
     end.
